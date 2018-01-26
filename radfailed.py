492f6e730ccd7ca9a10733c74aaf8337949f1ab1
Adriaan Dokter <adriaandokter@gmail.com>
2018-01-26 15:25:31 -0500

    check failed jobs in batch queue

---
+++ radfailed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import boto3
+client = boto3.client('batch')
+response=client.list_jobs(
+   jobQueue='spot40A',
+   jobStatus='FAILED',
+   maxResults=100
+)
+for item in response['jobSummaryList']:
+   print item['jobName']
+
+while 'nextToken' in response.keys():
+   response=client.list_jobs(
+      jobQueue='spot40A',
+      jobStatus='FAILED',
+      maxResults=100,
+      nextToken=response['nextToken']
+   )
+   for item in response['jobSummaryList']:
+      print item['jobName']
